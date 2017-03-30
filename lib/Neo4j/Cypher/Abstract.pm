94bdbd28f8d5b648f2d0ecd186a83a1c85086e4f
Mark A. Jensen <cpan@fortinbras.us>
2017-03-29 21:30:33 -0400

    feat(abstract): complete cypher syntax elts

--- lib/Neo4j/Cypher/Abstract.pm
+++ lib/Neo4j/Cypher/Abstract.pm
@@ -6,31 +6,50 @@ use warnings;
 # issues to solve:
 #  quoting
 #  parens
-my %dispatch;
+
+sub puke(@);
+sub belch(@);
+
 my @infix_binary = qw{
  - / % -in =~ = <> < > <= >=
- -contains -starts_with -ends_with
-};
+ -contains -starts_with -ends_with};
 my @infix_distributable = qw{ + * -and -or };
 my @prefix = qw{ -not };
 my @postfix = qw{ -is_null -is_not_null };
 my @function = qw{
+    ()
     -abs -ceil -floor -rand -round -sign
     -e -exp -log -log10 -sqrt -acos -asin -atan -atan2
     -cos -cot -haversin -pi -radians -sin -tan
     -left -lower -ltrim -replace -reverse -right
     -rtrim -split -substring -toString -trim -upper
- };
+    -length -size -type -id -coalesce -head -last
+    -labels -nodes -relationships -keys -tail -range};
+my @predicate = qw{ -all -any -none -single -filter};
+my @extract = qw{ -extract };
+my @reduce = qw{ -reduce };
 
+my %dispatch;
 @dispatch{@infix_binary} = (\&infix_binary) x @infix_binary;
 @dispatch{@infix_distributable} = (\&infix_distributable) x @infix_distributable;
 @dispatch{@prefix} = (\&prefix) x @prefix;
 @dispatch{@postfix} = (\&postfix) x @postfix;
 @dispatch{@function} = (\&function) x @function;
+@dispatch{@predicate} = (\&predicate) x @predicate;
+@dispatch{@extract} = (\&extract) x @extract;
+@dispatch{@reduce} = (\&reduce) x @reduce;
 
 sub new {
+  my $class = shift;
+  my ($dispatch) = @_ || \%dispatch;
+  if ($dispatch and !(ref $dispatch eq 'HASH')) {
+    puke "arg1 must be hashref mapping operators to coderefs (or absent)"
+  }
+  my $self = {
+    dispatch => $dispatch || {}
+   };
+  bless $self, $class;
 }
-
 
 sub belch (@) {
   my($func) = (caller(1))[3];
@@ -97,53 +116,73 @@ sub function {
   return _write_op($op).'('.join(',',@$args).')';
 }
 
+sub predicate {
+  my ($op, $args) = @_;
+  unless ($op and $args and !ref($op)
+	    and ref($args) eq 'ARRAY'){
+    puke "arg1 must be scalar, arg2 must be arrayref";
+  }
+  unless ( @$args == 3 ) {
+    puke "For $op, arg2 must have length 3";
+  }
+  return _write_op($op)."("."$$args[0] IN $$args[1] WHERE $$args[2]".")";
+}
+
+sub extract {
+  my ($op, $args) = @_;
+  unless ($op and $args and !ref($op)
+	    and ref($args) eq 'ARRAY'){
+    puke "arg1 must be scalar, arg2 must be arrayref";
+  }
+  unless ( @$args == 3 ) {
+    puke "For $op, arg2 must have length 3";
+  }
+  return _write_op($op)."("."$$args[0] IN $$args[1] | $$args[2]".")";
+}
+
+sub reduce {
+  my ($op, $args) = @_;
+  unless ($op and $args and !ref($op)
+	    and ref($args) eq 'ARRAY'){
+    puke "arg1 must be scalar, arg2 must be arrayref";
+  }
+  unless ( @$args == 5 ) {
+    puke "For $op, arg2 must have length 5";
+  }
+  return _write_op($op)."("."$$args[0] = $$args[1], $$args[2] IN $$args[3] | $$args[4]".")";
+}
+
 sub _write_op {
   my ($op) = @_;
   $op =~ s/^-//;
   my $c = (caller(1))[3];
-  return join(' ', map { ($c=~/function/) ? $_ : uc $_ } split /_/,$op);
-}
-1;
-
-package Neo4j::Cypher::Abstract::Peel;
-use Carp;
-use strict;
-use warnings;
-
-sub belch (@) {
-  my($func) = (caller(1))[3];
-  Carp::carp "[$func] Warning: ", @_;
+  return '' if ($op eq '()');
+  return join(' ', map { ($c=~/infix|prefix|postfix/) ? uc $_ : $_ } split /_/,$op);
 }
 
-sub puke (@) {
-  my($func) = (caller(1))[3];
-  Carp::croak "[$func] Fatal: ", @_;
-}
-
-sub new {
-  my $class = shift;
-  my ($dispatch) = @_;
-  if ($dispatch and !(ref $dispatch eq 'HASH')) {
-    puke "arg1 must be hashref mapping operators to coderefs (or absent)"
-  }
-  my $self = {
-    dispatch => $dispatch || {}
-   };
-  bless $self, $class;
-}
 
 # peel - recurse $args = [ -op, @args ] to create complete production
 sub peel {
   my ($self, $args) = @_;
-  #  state @acc;
-  return unless defined $args;
-  if (!ref $args) { # single literal argument
+
+  if (!defined $args) {
+    return '';
+  }
+  elsif (!ref $args) { # single literal argument
     return $args;
   }
   elsif (ref $args eq 'ARRAY') {
+    return '' unless (@$args);
     my $op = shift @$args;
     puke "'$op' : unknown operator" unless $self->{dispatch}{$op};
-    return $self->{dispatch}{$op}->( $op, [map { $self->peel($_) } @$args] );
+    my $expr = $self->{dispatch}{$op}->( $op, [map { $self->peel($_) } @$args] );
+    if (grep /\Q$op\E/, @infix_distributable) {
+      # group
+      return "($expr)"
+    }
+    else {
+      return $expr;
+    }
   }
   else {
     puke "Can only peel() arrayrefs or scalar literals";
